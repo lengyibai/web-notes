f32d179ac84fe52a31afdef5e79fd348d0538f38
lengyibai <1329670984@qq.com>
2022-01-29 02:31:57 +0800

    模板修改

--- Vue/Vue项目模板/src/App.vue
+++ Vue/Vue项目模板/src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div class="App"></div>
 </template>
-
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  name: 'App',
+  props: {},
+  data() {
+    return {};
   },
+  components: {},
+  methods: {},
 };
 </script>
-
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped lang="less">
+.App {
 }
 </style>
